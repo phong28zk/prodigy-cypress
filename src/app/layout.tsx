c3875c253ba0814d70a70406280bda21499843c4
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-11-19 16:25:15 +0700

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Archivo } from 'next/font/google'
 import './globals.css'
+import db from '@/lib/supabase/db'
 
-const inter = Inter({ subsets: ['latin'] })
+const font = Archivo({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,9 +15,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  console.log(db)
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
